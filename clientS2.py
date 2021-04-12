13bba5ef39f8489f92f34f260c599a4cbc6a81e6
TushieeV <41677179+TushieeV@users.noreply.github.com>
2021-04-12 22:59:57 +1000

    Update clientS2.py

--- clientS2.py
+++ clientS2.py
@@ -549,4 +549,4 @@ if __name__ == '__main__':
 
     #app.run(host='0.0.0.0', port='5000')
     #socketio.run(app, host='0.0.0.0', port=5000)
-    app.run(host='0.0.0.0')
+    app.run(host='0.0.0.0', port=80)
